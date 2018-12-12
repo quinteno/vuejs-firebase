14bef74104e8d9235e3eca77c655e97e806236f9
Quinten O'Neal <quintenoneal@Quintens-MacBook-Pro.local>
2018-12-11 19:45:36 -0500

    initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="login">
+    <h3>Sign In</h3>
+    <input type="text" placeholder="Email" /><br />
+    <input type="password" placeholder="Password" /><br />
+    <button>Connection</button>
+    <p>
+      Don't have an account? You can <router-link :to="{ name: 'SignUp', params: {} }">create one</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped> /* "scoped" limits the CSS to this component only */
+  .login {
+    margin-top: 40px;
+
+    input {
+      margin: 10px 0;
+    }
+
+    button {
+      margin-top: 20px;
+      width: 10%;
+      cursor: pointer;
+    }
+
+    p {
+      margin-top: 40px;
+      font-size: 13px;
+
+      a {
+        text-decoration: underline;
+        cursor: pointer;
+      }
+    }
+  }
+</style>
