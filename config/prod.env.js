4507dde30a4d54745104a6628810bfb86420718f
Quinten O'Neal <quintenoneal@Quintens-MacBook-Pro.local>
2019-01-06 16:36:42 -0500

    adding docs folder for gh pages

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,11 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+
+  FIREBASE_API_KEY: '"AIzaSyC2gmNhJFORgGkIm0UGESsaw8JU-fmE7MY"',
+  FIREBASE_AUTH_DOMAIN: '"drape-vuejs.firebaseapp.com"',
+  FIREBASE_DATABASE_URL: '"https://drape-vuejs.firebaseio.com"',
+  FIREBASE_PROJECT_ID: '"drape-vuejs"',
+  FIREBASE_STORAGE_BUCKET: '"gs://drape-vuejs.appspot.com"',
+  FIREBASE_MESSAGING_SENDER_ID: '"1058111268871"'
 }
