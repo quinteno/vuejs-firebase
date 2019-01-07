f361d2343c7b0837eb56b432d58fa2217e597902
Quinten O'Neal <quintenoneal@Quintens-MacBook-Pro.local>
2019-01-07 10:56:23 -0500

    paths

--- config/index.js
+++ config/index.js
@@ -49,8 +49,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
-    assetsSubDirectory: 'docs/static',
-    assetsPublicPath: './',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
