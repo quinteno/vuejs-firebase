d8c294e07f1265be87cdf2afac2256910f2fa138
Quinten O'Neal <qtylero@gmail.com>
2019-01-07 12:38:20 -0500

    Update dev.env.js

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,10 +5,10 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
 
-  FIREBASE_API_KEY: '"AIzaSyC2gmNhJFORgGkIm0UGESsaw8JU-fmE7MY"',
-  FIREBASE_AUTH_DOMAIN: '"drape-vuejs.firebaseapp.com"',
-  FIREBASE_DATABASE_URL: '"https://drape-vuejs.firebaseio.com"',
-  FIREBASE_PROJECT_ID: '"drape-vuejs"',
-  FIREBASE_STORAGE_BUCKET: '"gs://drape-vuejs.appspot.com"',
-  FIREBASE_MESSAGING_SENDER_ID: '"1058111268871"'
+  FIREBASE_API_KEY: '""',
+  FIREBASE_AUTH_DOMAIN: '""',
+  FIREBASE_DATABASE_URL: '""',
+  FIREBASE_PROJECT_ID: '""',
+  FIREBASE_STORAGE_BUCKET: '""',
+  FIREBASE_MESSAGING_SENDER_ID: '""'
 })
